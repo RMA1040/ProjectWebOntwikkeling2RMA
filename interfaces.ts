9335db0c0332d27bade3cd7b251fc8f83bfed181
Raymond Maetha wa-Meida <raymond.maethawa-meida@student.ap.be>
2025-02-13 20:15:22 +0100

    interfaces.ts update

---
+++ interfaces.ts
@@ -0,0 +1,30 @@
+import { Interface } from "readline"
+
+interface Driver{
+    id:string,
+    name:string,
+    description:string,
+    age:number,
+    driverNumber:number,
+    isActive:boolean,
+    birthdate:Date,
+    firstRace:Date,
+    teams:[string],
+    currentTeam:{
+        id:string,
+        name:string,
+        description:string,
+        dateOfExistence:Date,
+        amountOfChampionships:number,
+        amountOfWins:number
+    }
+}
+
+interface Team{
+    id:string,
+    name:string,
+    description:string,
+    dateOfExistence:Date,
+    amountOfChampionships:number,
+    amountOfWins:number
+}
