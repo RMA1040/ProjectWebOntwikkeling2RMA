6518728315fb04dfc77ed0a51ff80f4ef34a0591
Raymond Maetha wa-Meida <raymond.maethawa-meida@student.ap.be>
2025-05-08 16:38:43 +0000

    1st index

--- index.ts
+++ index.ts
@@ -1,28 +1,38 @@
 import express from "express";
-import {Driver, Team} from "./interfaces"
+import {Driver, Team} from "./interfaces";
 import driver from "./driver.json";
 import team from "./team.json";
 
 const app = express();
 
+app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.set("port", 3000);
-
-//-------------------------------------------------------------DRIVER ROUTES
 
 const drivers: Driver[] = driver;
 const teams: Team[] = team;
 
-app.get("/",(req,res)=>{
-    res.render("index");
+// Home route - nu met data
+app.get("/", (req, res) => {
+    res.render("index", {
+        drivers: drivers,
+        teams: teams
+    });
 });
 
-app.get("/drivers",(req,res)=>{
-    res.render("drivers");
+app.get("/drivers", (req, res) => {
+    res.render("drivers", {
+        drivers: drivers
+    });
 });
 
-app.get("/teams",(req,res)=>{
-    res.render("teams");
+app.get("/teams", (req, res) => {
+    res.render("teams", {
+        teams: teams,
+        drivers: drivers  // Nodig voor het tellen van actieve drivers per team
+    });
 });
 
-app.listen(app.get("port"), ()=>console.log( "[server] http://localhost:" + app.get("port")));
+app.listen(app.get("port"), () => {
+    console.log("[server] http://localhost:" + app.get("port"));
+});
