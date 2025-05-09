78c12c8adc9f95e9ec448542202d27d4e37b1621
Raymond Maetha wa-Meida <raymond.maethawa-meida@student.ap.be>
2025-05-09 11:08:22 +0000

    images fixed

--- interfaces.ts
+++ interfaces.ts
@@ -1,24 +1,25 @@
 import { Interface } from "readline"
 
 export interface Driver{
-    id:string,
-    name:string,
-    description:string,
-    age:number,
-    driverNumber:number,
-    nationality:string,
-    isActive:boolean,
-    birthdate:string, // of toch  "Date"?
-    firstRace:string, // of toch  "Date"?
-    teams:string[],
-    currentTeam:Team
+    id:string;
+    name:string;
+    imageUrl: string;
+    description:string;
+    age:number;
+    driverNumber:number;
+    nationality:string;
+    isActive:boolean;
+    birthdate:string; // of toch  "Date"?
+    firstRace:string; // of toch  "Date"?
+    teams:string[];
+    currentTeam:Team;
 }
 
 export interface Team{
-    id:string,
-    name:string,
-    description:string,
-    dateOfExistence:string, // of toch  "Date"?
-    amountOfChampionships:number,
-    amountOfWins:number
+    id:string;
+    name:string;
+    description:string;
+    dateOfExistence:string; // of toch  "Date"?
+    amountOfChampionships:number;
+    amountOfWins:number;
 }
